af595c0cc01db4668e23b6dbef14f69b417bd5a1
Kevin Palacios <palacios.coppiano.kevin@hotmail.com>
2022-12-07 22:27:39 -0500

    Validaciones en visual Mensajes en consola

--- src/utilitarios/FileUtilities.java
+++ src/utilitarios/FileUtilities.java
@@ -2,67 +2,76 @@ package utilitarios;
 
 import java.awt.Desktop;
 import java.io.*;
+import java.util.ArrayList;
 
 public class FileUtilities {
 
-	public String readFile(String FilePath) {
-		try{
-			//constructor of file class having file as argument  
-			File file=new File(FilePath);
-			FileInputStream fis = new FileInputStream(file);//opens a connection to an actual file  
-			System.out.println("file content: ");
-			int r=0;
-			while((r=fis.read())!=-1){
-				System.out.print((char)r);//prints the content of the file  
+	public String muestraContenido(String archivo) {
+		String cadena = "";
+		String aux = "";
+		FileReader f;
+		BufferedReader b;
+		try {
+			f = new FileReader(archivo);
+			b = new BufferedReader(f);
+			while ((cadena = b.readLine()) != null) {
+				aux += cadena;
 			}
-		}catch(Exception e){
+			b.close();
+			return aux;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
 		}
-		return "";
 	}
 	
-	public static void muestraContenido(String archivo) throws FileNotFoundException, IOException { 
-    	String cadena; 
-        FileReader f = new FileReader(archivo); 
-        BufferedReader b = new BufferedReader(f); 
-        while((cadena = b.readLine())!=null) { 
-        	System.out.println(cadena); 
-        } 
-        b.close(); 
-	} 
-
-	public String readFile2(String FilePath) {
-		File file;
+	public boolean crearArchivo(String nuevoArchivo, ArrayList<String> contenido) {
+		FileWriter flwriter = null;
 		try {
-			file = new File(FilePath);
-			if (!Desktop.isDesktopSupported()) {
-				System.out.println("not supported");
-				return "";
+			//crea el flujo para escribir en el archivo
+			flwriter = new FileWriter(nuevoArchivo);
+			//crea un buffer o flujo intermedio antes de escribir directamente en el archivo
+			BufferedWriter bfwriter = new BufferedWriter(flwriter);
+			for (String linea : contenido) {
+				//escribe los datos en el archivo
+				bfwriter.write(linea+"\n");
+				System.out.println("linea: "+linea);
 			}
-			Desktop desktop = Desktop.getDesktop();
-			if (file.exists())// checks file exists or not  
-				desktop.open(file);// opens the specified file  
-		} catch (Exception e) {
+			//cierra el buffer intermedio
+			bfwriter.close();
+			System.out.println("Archivo creado satisfactoriamente..");
+			return true;
+		} catch (IOException e) {
 			e.printStackTrace();
+			return false;
+		} finally {
+			if (flwriter != null) {
+				try {//cierra el flujo principal
+					flwriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return false;
+			}
 		}
-		return "";
-	}
-
-	public String saveFile() {
-		return "";
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String ruta = "C:\\Users\\Kevin Palacios\\Documents\\contraseña.txt";
+		String ruta2 = "C:\\Users\\Kevin Palacios\\Documents\\mensaje.txt";
 		FileUtilities fu = new FileUtilities();
-		fu.readFile(ruta);
-		try {
-			fu.muestraContenido(ruta);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String a = fu.muestraContenido(ruta);
+		System.out.println(a);
+		
+		ArrayList<String> contenido = new ArrayList<String>();
+		contenido.add("la vida es hermosa");
+		fu.crearArchivo(ruta2, contenido);
 	}
 
 }
--- src/visual/AesVisual.java
+++ src/visual/AesVisual.java
@@ -1,14 +1,17 @@
 package visual;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
@@ -19,14 +22,23 @@ import javax.swing.JTextPane;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import algoritmo.AES;
+import utilitarios.FileUtilities;
+
 import java.awt.TextArea;
 import java.awt.Button;
+import java.awt.Color;
 
 public class AesVisual {
 
 	private JFrame frame;
 	private JTextField textKey;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private FileUtilities tools = new FileUtilities();
+	private AES aes = new AES();
+	private String contenido = "";
+	private boolean esCifrado = false;
+	private boolean esDescifrado = false;
 
 	/**
 	 * Launch the application.
@@ -59,8 +71,25 @@ public class AesVisual {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-		
+
+		JLabel lblStatus = new JLabel("Status");
+		lblStatus.setBounds(128, 36, 46, 14);
+		frame.getContentPane().add(lblStatus);
+
+		textKey = new JTextField();
+		textKey.setBounds(22, 158, 152, 20);
+		frame.getContentPane().add(textKey);
+		textKey.setColumns(10);
+
+		JLabel lblNewLabel = new JLabel("Llave: ");
+		lblNewLabel.setBounds(22, 140, 46, 14);
+		frame.getContentPane().add(lblNewLabel);
+
+		TextArea textArea = new TextArea();
+		textArea.setEditable(false);
+		textArea.setBounds(191, 32, 221, 146);
+		frame.getContentPane().add(textArea);
+
 		JButton btnLoadFile = new JButton("Load File");
 		btnLoadFile.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -69,78 +98,122 @@ public class AesVisual {
 				final JFileChooser fc = new JFileChooser();
 				fc.setFileFilter(filter);
 				int seleccion = fc.showOpenDialog(btnLoadFile);
-				System.out.println(seleccion);
-				if (seleccion == JFileChooser.APPROVE_OPTION){
-				   File fichero = fc.getSelectedFile();
-				   // Aquí debemos abrir y leer el fichero.
-				   System.out.println(fichero.getAbsolutePath());
-				  
+				System.out.println("Press Load File...seleccion: "+seleccion);
+				if (seleccion == JFileChooser.APPROVE_OPTION) {
+					File fichero = fc.getSelectedFile();
+					// Aquí debemos abrir y leer el fichero.
+					System.out.println("Press Load File...path: "+fichero.getAbsolutePath());
+					contenido = tools.muestraContenido(fichero.getAbsolutePath());
+					lblStatus.setText(" OK");
+					lblStatus.setOpaque(true);
+					lblStatus.setForeground(Color.WHITE);
+					lblStatus.setBackground(Color.GREEN);
 				}
-				
 			}
 		});
 		btnLoadFile.setBounds(22, 32, 89, 23);
 		frame.getContentPane().add(btnLoadFile);
-		
-		JLabel lblStatus = new JLabel("Status");
-		lblStatus.setBounds(128, 36, 46, 14);
-		frame.getContentPane().add(lblStatus);
-		
+
 		JRadioButton rdbtnCifrar = new JRadioButton("Cifrar");
 		rdbtnCifrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Press Cifrar...");
+				esCifrado = true;
+				System.out.println("Press Cifrar...esCifrado:" + esCifrado);
+				esDescifrado = false;
+				System.out.println("Press Cifrar...esDescifrado:" + esDescifrado);
+
 			}
 		});
 		buttonGroup.add(rdbtnCifrar);
 		rdbtnCifrar.setBounds(22, 76, 109, 23);
 		frame.getContentPane().add(rdbtnCifrar);
-		
+
 		JRadioButton rdbtnDecifrar = new JRadioButton("Decifrar");
 		rdbtnDecifrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Press Decifrar...");
+				esDescifrado = true;
+				System.out.println("Press Decifrar...esDescifrado:" + esDescifrado);
+				esCifrado = false;
+				System.out.println("Press Decifrar...esCifrado:" + esCifrado);
 			}
 		});
 		buttonGroup.add(rdbtnDecifrar);
 		rdbtnDecifrar.setBounds(22, 110, 109, 23);
 		frame.getContentPane().add(rdbtnDecifrar);
-		
-		textKey = new JTextField();
-		textKey.setBounds(22, 158, 152, 20);
-		frame.getContentPane().add(textKey);
-		textKey.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Llave: ");
-		lblNewLabel.setBounds(22, 140, 46, 14);
-		frame.getContentPane().add(lblNewLabel);
-		
+
 		JButton btnExport2File = new JButton("Export");
 		btnExport2File.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Press Export...");
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("TXT", "txt");
+				final JFileChooser fc = new JFileChooser();
+				ArrayList<String> pantalla = new ArrayList<String>();
+				fc.setFileFilter(filter);
+				int seleccion = fc.showSaveDialog(btnLoadFile);
+				boolean respuesta = false;
+				System.out.println("Press Load File...seleccion: "+seleccion);
+				if (seleccion == JFileChooser.APPROVE_OPTION) {
+					File fichero = fc.getSelectedFile();
+					// Aquí debemos abrir y leer el fichero.
+					System.out.println("Press Export...path: "+fichero.getAbsolutePath()+".txt");
+					pantalla.add(textArea.getText());
+					respuesta = tools.crearArchivo(fichero.getAbsolutePath()+".txt", pantalla);
+				}
 			}
 		});
 		btnExport2File.setBounds(191, 189, 89, 23);
 		frame.getContentPane().add(btnExport2File);
-		
+
 		JButton btnGo = new JButton("Go");
 		btnGo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Press Go...");
+				String Key = textKey.getText();
+				System.out.println("Press Go...Key:" + Key);
+				System.out.println("Press Go...esCifrado:" + esCifrado);
+				System.out.println("Press Go...esDescifrado:" + esDescifrado);
+				if (contenido.length() == 0) {
+					JOptionPane.showMessageDialog(null, "No ha seleccionado un archivo", "Archivo vacío",
+							JOptionPane.WARNING_MESSAGE);
+				} else if (!esCifrado & !esDescifrado) {
+					JOptionPane.showMessageDialog(null, "No hay seleccionado una opcion:\n Cifrar o Descifrar",
+							"Opcion vacía", JOptionPane.WARNING_MESSAGE);
+				} else if (Key.length() == 0) {
+					JOptionPane.showMessageDialog(null, "La llave está vacia", "Llave vacía",
+							JOptionPane.WARNING_MESSAGE);
+				} else {
+					System.out.println("Press Go...habilitar Export: true");
+					btnExport2File.setEnabled(true);
+					aes.KEY(Key);
+					if (esCifrado)
+						textArea.setText(aes.cifrado(contenido));
+
+					if (esDescifrado)
+						textArea.setText(aes.decifrado(contenido));
+				}
+
 			}
 		});
 		btnGo.setBounds(58, 189, 89, 23);
 		frame.getContentPane().add(btnGo);
-		
-		TextArea textArea = new TextArea();
-		textArea.setBounds(191, 32, 221, 146);
-		frame.getContentPane().add(textArea);
-		
-		JButton btnClean = new JButton("Clean");
+
+		JButton btnClean = new JButton("Clear");
 		btnClean.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Press Clean...");
+				contenido = "";
+				lblStatus.setOpaque(false);
+				lblStatus.setForeground(Color.BLACK);
+				lblStatus.setText("Status");
+				esCifrado = false;
+				rdbtnCifrar.setSelected(false);
+				esDescifrado = false;
+				rdbtnDecifrar.setSelected(false);
+				textArea.setText("");
+				textKey.setText("");
+				btnExport2File.setEnabled(false);
 			}
 		});
 		btnClean.setBounds(323, 189, 89, 23);
